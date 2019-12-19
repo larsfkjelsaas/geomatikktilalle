65ff06f507bd8e3b51b276af03f76b2783348c5d
Hephlathio <hephlathio@hotmail.com>
2019-12-19 17:36:32 +0100

    small ui change to allow for smaller screens

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export default styled.div`
   display: grid;
   height: 100vh;
-  grid-template-columns: 20% 250px 1fr;
+  grid-template-columns: 400px 250px 1fr;
   grid-template-rows: 10% 1fr;
 `;
