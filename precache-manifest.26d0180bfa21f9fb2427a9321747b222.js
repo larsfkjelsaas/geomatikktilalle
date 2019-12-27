8f7b2dfc3a7bb5e4b3f46bd6cb671809f89caefd
Hephlathio <hephlathio@hotmail.com>
2019-12-27 11:57:57 +0100

    Updates

---
+++ precache-manifest.26d0180bfa21f9fb2427a9321747b222.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e841c05c8432681e06078e777e59266c",
+    "url": "/geomatikk-til-alle/index.html"
+  },
+  {
+    "revision": "03fd826f1c80d5b358bc",
+    "url": "/geomatikk-til-alle/static/js/2.a8329984.chunk.js"
+  },
+  {
+    "revision": "bc13fb4f18e251769526aa1eb180a0e6",
+    "url": "/geomatikk-til-alle/static/js/2.a8329984.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dae87a02c919f588ee4",
+    "url": "/geomatikk-til-alle/static/js/main.41815828.chunk.js"
+  },
+  {
+    "revision": "497a368e4e7dd8ee5b0d",
+    "url": "/geomatikk-til-alle/static/js/runtime-main.6f943ac9.js"
+  }
+]);
